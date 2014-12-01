b5bf97cab510a1b73a0922d9d0d944b298c283e4
mkubarewicz <mkubarewicz@users.noreply.github.com>
2014-11-30 19:07:19 -0500

    Update Times.class.php
    Made more list group friendly

--- Classes/Times.class.php
+++ Classes/Times.class.php
@@ -7,7 +7,6 @@ require_once 'DB.class.php';
 //Adapted from https://github.com/revdrbrown/StoryTrees/blob/master/classes/User.class.php
 //and https://github.com/revdrbrown/StoryTrees/blob/master/classes/UserTools.class.php
 //
-
 class TimeClass {
 
 	// Times Table Class Properties
@@ -35,30 +34,75 @@ class TimeClass {
 		$this->userid = (isset($data['userid'])) ? $data['userid'] : "";
 		
 	}
-		
+
+	
 	//Insert new time into the database.
 	public function insert($data) {
-		$db = $this->db;
+		$db = new DB();
 		$this->ID = $db->insert($data, 'times');
 		return true;
 	}
 	
+	
 	//Update time in the database where time ids match.
-	public function update(TimeClass $data, $tID) {
-		$db = $this->db;
-		foreach ($tID as $row) {
-			$this->ID = $db->update($data, "times", 'id = ' . $row);
+	public function update($data, $tID) {
+		$db = new DB();
+		foreach ($data as $row) {
+			$this->ID = $db->update($data, "times", 'id = ' . $tID);
 		}
 		return true;
 	}
 	
+	
 	//Delete time from the database.
 	public function delete($tID) {
-		$db = $this->db;
+		$db = new DB();
         $db->delete("times", 'id = '. $tID);
 
 		return true;
 	}	
+		
+		
+	//Get the time ID record from the database.
+	public function get($tID) {
+		$db = new DB();
+		$result = $db->select("*", "times", "id = $tID");
+		
+		return new TimeClass($result);
 	}
-
+   
+   
+	//Show a list group of all the times for a user using links.
+    	public function showTimesList($whereTo = "#", $userid) {
+		$db = new DB();
+		
+		//Select all the fields for a user and order by Distance, Stroke, then Time.
+		 $rows = $db->select("*", "times", "userid = $userid", "Distance, Stroke, Times"); 
+            
+		//If they have no records, display error message.
+		 if ($db->numRows == 0)
+            		echo "There are no swim times submitted yet.";    
+       
+		//If they have only one record, print with list group.
+		elseif ($db->numRows == 1) {
+			echo '<a href="#" class="list-group-item active"> Name &emsp;&emsp; Gender &emsp;&emsp;' .
+				'Age Range &emsp;&emsp; Distance &emsp;&emsp; Stroke &emsp;&emsp; Time </a>';
+            		echo '<a href="' . $whereTo . '?tID=' . $rows["ID"] . '" class="list-group-item">' .
+				$rows["Name"] . "&emsp;&emsp;&emsp;" . $rows["Gender"] . "&emsp;&emsp;&emsp;" . $rows["Age_Range"] . 
+				"&emsp;&emsp;&emsp;" . $rows["Distance"] . "&emsp;&emsp;&emsp;" . $rows["Stroke"] . 
+				"&emsp;&emsp;&emsp;" . $rows["Times"] . "</a>";
+        	}
+		//If they have more than one record, print list group using an array.
+		  else {
+			echo '<a href="#" class="list-group-item active"> Name &emsp;&emsp; Gender &emsp;&emsp;' .
+				'Age Range &emsp;&emsp; Distance &emsp;&emsp; Stroke &emsp;&emsp; Time </a>';
+            		foreach($rows as $row) {
+        		 echo '<a href="' . $whereTo . '?tID=' . $row["ID"] . '" class="list-group-item">' .
+				$row["Name"] . "&emsp;&emsp;&emsp;" . $row["Gender"] . "&emsp;&emsp;&emsp;" . $row["Age_Range"] . 
+				"&emsp;&emsp;&emsp;" . $row["Distance"] . "&emsp;&emsp;&emsp;" . $row["Stroke"] . 
+				"&emsp;&emsp;&emsp;" . $row["Times"] . "</a>";
+            }
+        }
+    }	
+}
 ?>
